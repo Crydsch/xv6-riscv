0a2bd48fb5d94c8270bd71dd2a2268806d42f82b
Christoph Wagner <wagnerc@in.tum.de>
2021-05-19 13:27:46 +0200

    fixed function declaration

--- kernel/vm.h
+++ kernel/vm.h
@@ -6,7 +6,7 @@
 void            kvminit(void);
 void            kvminithart(void);
 uint64          kvmpa(uint64);
-void            kvmmap(uint64, uint64, uint64, int);
+void            kvmmap(pagetable_t, uint64, uint64, uint64, int);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
